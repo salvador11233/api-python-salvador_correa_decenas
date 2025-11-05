1959a47347217bf8c9a8097607f03012fdb79441
Salvador <salvadorcorreadecenas@gmail.com>
2025-11-05 04:58:13 -0600

    CO: Correción en serializers
    Se realiza corrección en el Serializers, se tenia una s de más

--- api/serializers.py
+++ api/serializers.py
@@ -1,12 +1,12 @@
 from rest_framework import serializers
 from .models import Usuarios, Productos
 
-class UsuariosSerializers(serializers.ModelSerializers):
+class UsuariosSerializers(serializers.ModelSerializer):
     class Meta:
         model = Usuarios
         fields = '__all__'
 
-class ProductosSerializers(serializers.ModelSerializers):
+class ProductosSerializers(serializers.ModelSerializer):
     class Meta:
         model = Productos
         fields = '__all__'
