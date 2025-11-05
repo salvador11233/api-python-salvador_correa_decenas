f29b374a391d9c62df57e4d8993bb5f3a5012d33
Salvador <salvadorcorreadecenas@gmail.com>
2025-11-05 04:34:28 -0600

    Me: Actualización de Urls
    Se añaden y se actualiza las Urls para poder acceder al EndPoint

---
+++ api/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .views import(UsuarioLista, UsuarioDetalle, ProductoLista, ProductoDetalle)
+
+urlpatterns = [
+    # URLs de Usuarios sin ID
+    path('usuarios', UsuarioLista.as_view(), name='usuario_lista'),
+    # URLs de Usuario con ID
+    path('usuarios/<int:pk>', UsuarioDetalle.as_view(), name='usuario_detalle'),
+
+    # URLs de Productos sin ID
+    path('productos', ProductoLista.as_view(), name='producto_lista'),
+    # URLs de Producto con ID
+    path('prodcutos', ProductoDetalle.as_view(), name='producto_detalle'),
+
+]
