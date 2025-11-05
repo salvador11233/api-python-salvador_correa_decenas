7e04950784cc510a74fe80f80c1ef64a640eb1e3
Salvador <salvadorcorreadecenas@gmail.com>
2025-11-05 02:09:57 -0600

    Nvo: Se añade Modelos
    Se añade Modelos de usuarios y productos en la REST API

--- api/models.py
+++ api/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+# Se crea Modelo de Usuarios
+class Usuarios(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    edad = models.IntegerField()
+    activo = models.BooleanField(default=True)
+    created = models.DateField(auto_now_add=True)
+    last_update = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.apellido} {self.apellido}"
+
+# Se crea Modelo de producto
+class Productos(models.Model):
+    nombre = models.CharField(max_length=150)
+    precio = models.DecimalField(max_digits=10,decimal_places=2) 
+    stock = models.PositiveBigIntegerField()
+    activo = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nombre
+
